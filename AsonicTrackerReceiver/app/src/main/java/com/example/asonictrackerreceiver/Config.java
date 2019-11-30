c04100dc69c3ae9d63de8223a05f18567996feda
LeeDa16 <lishuai16THU@163.com>
2019-11-30 16:04:00 +0800

    add some code about 2D tracking, but not finish

--- AsonicTrackerReceiver/app/src/main/java/com/example/asonictrackerreceiver/Config.java
+++ AsonicTrackerReceiver/app/src/main/java/com/example/asonictrackerreceiver/Config.java
@@ -2,14 +2,18 @@ package com.example.asonictrackerreceiver;
 
 public class Config {
     public static final int SamplingRate = 48000;
-    public static final int StartFreq = 17000;
-    public static final int EndFreq = 19000;
+    public static final int StartFreqA = 17000;
+    public static final int EndFreqA = 19000;
+    public static final int StartFreqB = 14000;
+    public static final int EndFreqB = 16000;
     public static final double T = 0.04;
-    public static final int BandPassCenter = 18000;
+    public static final int BandPassCenterA = 18000;
+    public static final int BandPassCenterB = 15000;
     public static final int BandPassOffset = 2000;
     public static final int StartThreshold = 50;
     public static final int SoundSpeed = 340;
     public static final int FMCW_FFTLen = 1024 * 64;
+    public static final double SpeakerDist = 0.5;
 
     public static final int SampleNum = 1 + (int)(T * SamplingRate);
 }
--- AsonicTrackerReceiver/app/src/main/java/com/example/asonictrackerreceiver/Recorder.java
+++ AsonicTrackerReceiver/app/src/main/java/com/example/asonictrackerreceiver/Recorder.java
@@ -6,6 +6,8 @@ import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.util.Log;
 
+import java.util.concurrent.ConcurrentNavigableMap;
+
 interface CallBack {
     void solve_distance(double dis);
 }
@@ -18,8 +20,10 @@ public class Recorder extends Thread {
     private AudioRecord audioRecord;
     private int bufferSize;
     private byte[] buffer;
-    private BandPassFilter bandPassFilter;
-    private FMCW fmcw;
+    private BandPassFilter bandPassFilterA;
+    private FMCW fmcwA;
+    private BandPassFilter bandPassFilterB;
+    private FMCW fmcwB;
     CallBack callBack;
 
     public boolean recording = false;
@@ -32,8 +36,10 @@ public class Recorder extends Thread {
         audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SamplingRate,
                 channelConfiguration, audioEncoding, bufferSize * 50);
         buffer = new byte[bufferSize];
-        bandPassFilter = new BandPassFilter(Config.BandPassCenter, Config.BandPassOffset, Config.SamplingRate);
-        fmcw = new FMCW(Config.SamplingRate, Config.T, Config.StartFreq, Config.EndFreq);
+        bandPassFilterA = new BandPassFilter(Config.BandPassCenterA, Config.BandPassOffset, Config.SamplingRate);
+        fmcwA = new FMCW(Config.SamplingRate, Config.T, Config.StartFreqA, Config.EndFreqA);
+        bandPassFilterB = new BandPassFilter(Config.BandPassCenterB, Config.BandPassOffset, Config.SamplingRate);
+        fmcwB = new FMCW(Config.SamplingRate, Config.T, Config.StartFreqB, Config.EndFreqB);
         callBack = call_back;
     }
 
@@ -43,8 +49,10 @@ public class Recorder extends Thread {
             // Main work for recording
             recording = true;
             audioRecord.startRecording();
-            int last_tail = 0;
-            boolean aligned = false;
+            int last_tail_a = 0;
+            int last_tail_b = 0;
+            boolean aligned_a = false;
+            boolean aligned_b = false;
             int read_cnt = 0;
             while (recording) {
                 int num_rec = audioRecord.read(buffer, 0, bufferSize);
@@ -53,28 +61,66 @@ public class Recorder extends Thread {
                 if (num_rec != 0 && num_rec != -1) {
                     // do signal process here
                     double[] audio = Utils.bytes2double(buffer, num_rec);
-                    double[] receive = bandPassFilter.filter(audio);
-                    int start_pos;
-                    if (aligned) start_pos = 2 * Config.SampleNum - last_tail;
-                    else {
-                        start_pos = Utils.findStart(receive);
-                        if (start_pos >= 0) aligned = true;
+                    double[] receive_a = bandPassFilterA.filter(audio);
+                    double[] receive_b = bandPassFilterB.filter(audio);
+                    int start_pos_a = -1;
+                    int start_pos_b = -1;
+//                    if (aligned) start_pos = 2 * Config.SampleNum - last_tail;
+//                    else {
+//                        start_pos = Utils.findStart(receive);
+//                        if (start_pos >= 0) aligned = true;
+//                    }
+                    if (aligned_a && aligned_b) {
+                        start_pos_a = 2 * Config.SampleNum - last_tail_a;
+                        start_pos_b = 2 * Config.SampleNum - last_tail_b;
+                    } else if (aligned_a) {
+                        start_pos_b = Utils.findStart(receive_b);
+                        if (start_pos_b >= 0) aligned_b = true;
+                        if (start_pos_a >= 0) {
+                            while (start_pos_a + Config.SampleNum * 2 <= receive_a.length) {
+                                start_pos_a += Config.SampleNum * 2;
+                            }
+                            last_tail_a = receive_a.length - start_pos_a;
+                        }
+                    } else if (aligned_b) {
+                        start_pos_a = Utils.findStart(receive_a);
+                        if (start_pos_a >= 0) aligned_a = true;
+                        if (start_pos_b >= 0) {
+                            while (start_pos_b + Config.SampleNum * 2 <= receive_b.length) {
+                                start_pos_b += Config.SampleNum * 2;
+                            }
+                            last_tail_b = receive_b.length - start_pos_b;
+                        }
+                    } else {
+                        start_pos_a = Utils.findStart(receive_a);
+                        if (start_pos_a >= 0) aligned_a = true;
+                        start_pos_b = Utils.findStart(receive_b);
+                        if (start_pos_b >= 0) aligned_b = true;
                     }
 
-                    if (start_pos >= 0) {
-                        // Log.i("START_POS", String.format("last tail is: %d, new start is: %d", last_tail, start_pos));
-                        // boolean caled = false;
-                        while (start_pos + Config.SampleNum * 2 <= receive.length) {
-                            // if (!caled) {
-                            double dis = fmcw.delta_dis(receive, start_pos);
-                            callBack.solve_distance(dis);
-                            // caled = true;
-                            //}
-                            start_pos += Config.SampleNum * 2;
+                    if (start_pos_a >= 0 && start_pos_b >= 0) {
+                        while (start_pos_a + Config.SampleNum * 2 <= receive_a.length && start_pos_b + Config.SampleNum * 2 <= receive_b.length) {
+                            double dis_a = fmcwA.delta_dis(receive_a, start_pos_a);
+                            double dis_b = fmcwB.delta_dis(receive_b, start_pos_b);
+                            double x = (Config.SpeakerDist * Config.SpeakerDist + dis_a * dis_a - dis_b * dis_b) / (2 * Config.SpeakerDist);
+                            double y = Math.sqrt(dis_a * dis_a - x * x);
                         }
-                        last_tail = receive.length - start_pos;
-                        read_cnt++;
                     }
+
+//                    if (start_pos >= 0) {
+//                        // Log.i("START_POS", String.format("last tail is: %d, new start is: %d", last_tail, start_pos));
+//                        // boolean caled = false;
+//                        while (start_pos + Config.SampleNum * 2 <= receive.length) {
+//                            // if (!caled) {
+//                            double dis = fmcwA.delta_dis(receive, start_pos);
+//                            callBack.solve_distance(dis);
+//                            // caled = true;
+//                            //}
+//                            start_pos += Config.SampleNum * 2;
+//                        }
+//                        last_tail = receive.length - start_pos;
+//                        read_cnt++;
+//                    }
                 }
             }
             audioRecord.stop();
